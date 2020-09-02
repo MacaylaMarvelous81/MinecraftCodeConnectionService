aa01dc8f643d13a5755a7589a0ab20f3bc4d56b2
kurvy <admin@thelegendaryorb.tk>
2020-09-01 19:55:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 var request = require("request")
 (async function() {
   console.log("Waiting for response from Code Connection...")
-  let connected = await request("localhost:8080/connected")
+  let connected = await request({
+    method: "GET",
+    uri: "localhost:8080/connected"
+  })
   if(connected.body === "true") {
     console.log("Connected!")
   } else {
     console.log("Unable to connect.")
     return
   }
-  await request("localhost:8080/move?direction=forward")
-  await request("localhost:8080/move?direction=forward")
-  await request("localhost:8080/move?direction=back")
-  await request("localhost:8080/turn?direction=left")
-  await request("localhost:8080/turn?direction=right")
+  await request({
+    method: "GET",
+    uri: "localhost:8080/move?direction=forward")
+  })
+  await request({
+    method: "GET",
+    uri: "localhost:8080/move?direction=forward")
+  })
+  await request({
+    method: "GET",
+    uri: "localhost:8080/move?direction=back")
+  })
+  await request({
+    method: "GET",
+    uri: "localhost:8080/turn?direction=left")
+  })
+  await request({
+    method: "GET",
+    uri: "localhost:8080/move?turn?direction=right")
+  })
 })()
