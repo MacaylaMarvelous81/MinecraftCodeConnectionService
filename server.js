b54c93623f9ea8d4e3a28c1e085bcd006ca99c57
kurvy <admin@thelegendaryorb.tk>
2020-09-01 20:09:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var request = require("request")
 async function iife1() {
   console.log("Waiting for response from Code Connection...")
+  /*
   let connected = await request("http://localhost:8080/connected")
   if(connected.body.includes("true")) {
     console.log("Connected!")
@@ -8,6 +9,7 @@ async function iife1() {
     console.log("Unable to connect.")
     return
   }
+  */
   await request("http:/localhost:8080/move?direction=forward")
   await request("http:/localhost:8080/move?direction=forward")
   await request("http:/localhost:8080/move?direction=back")
